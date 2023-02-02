4355ed68c32e9e3e3cbc7bf5caae5368452773fb
Edwaldo Utama <53479749+edwaldo12@users.noreply.github.com>
2023-02-02 21:29:03 +0700

    Update Form.js

--- frontend/src/components/Form/Form.js
+++ frontend/src/components/Form/Form.js
@@ -33,7 +33,7 @@ const Form = ({ props }) => {
                 Username
               </label>
               <input
-                className="shadow appearance-none border border-red-500 rounded w-full py-2 px-3 text-gray-700 mb-3 leading-tight focus:outline-none focus:shadow-outline"
+                className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 mb-3 leading-tight focus:outline-none focus:shadow-outline"
                 id="username"
                 type="text"
                 placeholder="Username"
