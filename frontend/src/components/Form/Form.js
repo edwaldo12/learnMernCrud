6ce1686e77970871ca3d4397113219b14eaeafda
Edwaldo Utama <53479749+edwaldo12@users.noreply.github.com>
2023-02-24 23:09:51 +0700

    progress

--- frontend/src/components/Form/Form.js
+++ frontend/src/components/Form/Form.js
@@ -67,7 +67,7 @@ const Form = ({ props }) => {
                 Password
               </label>
               <input
-                className="shadow appearance-none border border-red-500 rounded w-full py-2 px-3 text-gray-700 mb-3 leading-tight focus:outline-none focus:shadow-outline"
+                className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 mb-3 leading-tight focus:outline-none focus:shadow-outline"
                 id="password"
                 type="password"
                 placeholder="Password"
@@ -99,7 +99,7 @@ const Form = ({ props }) => {
                 Password
               </label>
               <input
-                className="shadow appearance-none border border-red-500 rounded w-full py-2 px-3 text-gray-700 mb-3 leading-tight focus:outline-none focus:shadow-outline"
+                className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 mb-3 leading-tight focus:outline-none focus:shadow-outline"
                 id="password"
                 type="password"
                 placeholder="Password"
